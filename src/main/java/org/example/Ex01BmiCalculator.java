4b5b45c299b91dcddaaeea83c98507fa1e0cf46f
kamil.klimaszewski@gmail.com <kamil.klimaszewski@gmail.com>
2023-04-22 21:48:56 +0200

    BMI Implementation with JUnit

---
+++ src/main/java/org/example/Ex01BmiCalculator.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Ex01BmiCalculator {
+    public static double calculateBmi(double weight, double height) {
+        if (weight <= 0 || height <= 0) {
+            throw new IllegalArgumentException("Weight and height must be positive numbers");
+        }
+        double bmi = weight / (height * height);
+        return Math.round(bmi * 100) / 100.0;
+    }
+}
---
+++ src/test/java/org/example/Ex01BmiCalculatorTest.java
@@ -0,0 +1,52 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class Ex01BmiCalculatorTest {
+    @Test
+    void shouldCalculateBmi() {
+        double weight = 70.0;
+        double height = 1.75;
+        double expectedBmi = 22.86;
+
+        double actualBmi = Ex01BmiCalculator.calculateBmi(weight, height);
+
+        assertEquals(expectedBmi, actualBmi, 0.01);
+    }
+
+    @Test
+    void shouldThrowExceptionForNotPositiveWeight() {
+        double weight = -70.0;
+        double height = 1.75;
+
+        assertThrows(IllegalArgumentException.class, () -> Ex01BmiCalculator.calculateBmi(weight, height));
+    }
+
+    @Test
+    void shouldThrowExceptionForNotPositiveHeight() {
+        double weight = 70.0;
+        double height = -1.75;
+
+        assertThrows(IllegalArgumentException.class, () -> Ex01BmiCalculator.calculateBmi(weight, height));
+    }
+
+    @Test
+    void shouldThrowExceptionForWeightEqualZero() {
+        double weight = 0;
+        double height = 1.75;
+
+        assertThrows(IllegalArgumentException.class, () -> Ex01BmiCalculator.calculateBmi(weight, height));
+    }
+
+    @Test
+    void shouldThrowExceptionForHeightEqualZero() {
+        double weight = 70.0;
+        double height = 0;
+
+        assertThrows(IllegalArgumentException.class, () -> Ex01BmiCalculator.calculateBmi(weight, height));
+    }
+
+}
